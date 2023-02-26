23510b3589d6c5d848fbdaa6a7c1dc5639a3a0ae
VolynetsOleksii <volinetsaleksey@gmail.com>
2023-02-26 17:28:06 +0200

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
+// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
+
+// Додай бібліотеку SimpleLightbox як залежність проекту, використовуючи npm (посилання на CDN з 
+// твоєї минулої роботи більше не потрібне).
+// Використовуй свій JavaScript код з попередньої домашньої роботи, але виконай рефакторинг з 
+// урахуванням того, що бібліотека була встановлена через npm (синтаксис import/export).
+// Для того щоб підключити CSS код бібліотеки в проект, необхідно додати ще один імпорт, крім того, 
+// що описаний в документації.
+
+// // Описаний в документації
+// import SimpleLightbox from "simplelightbox";
+// // Додатковий імпорт стилів
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
+
+const divGalleryRef = document.querySelector(".gallery");
+
+const galleryMarkupRef = createGalleryMarkup(galleryItems);
+
+divGalleryRef.insertAdjacentHTML("beforeend", galleryMarkupRef);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+        `;
+    })
+    .join("");
+}
+new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt' });
 // Change code below this line
 
 console.log(galleryItems);
